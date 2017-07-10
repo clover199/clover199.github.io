dc4c18e882b620150657f9ff0d14eb9a6dccc2ed
Clover <zzhhyyee@hotmail.com>
2017-07-10 14:28:41 -0500

    revert to simplest case

--- script.js
+++
@@ -1,8 +0,0 @@
-function show_nav_bar() {
-    var x = document.getElementById('navlist');
-    if (x.className === 'menue') {
-      x.className = 'menue_show';
-    } else {
-      x.className = 'menue';
-    }
-}
