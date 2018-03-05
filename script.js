034ef9873b52bce46db1844434db44ad066f8661
clover199 <zzhhyyee@hotmail.com>
2018-03-05 15:06:38 -0600

    added table of contents

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// function to show or hide navigation bar
 function show_nav_bar() {
     var x = document.getElementById('navlist');
     if (x.className === 'menue') {
@@ -6,3 +7,29 @@ function show_nav_bar() {
       x.className = 'menue';
     }
 }
+
+// create table of contents automatically
+$(document).ready(function(){
+  var ToC =
+    "<section role='navigation' class='table-of-contents' " +
+    "style='width:30%;position:fixed;margin:60px 70%;'>" +
+      "<h4>Table of Contents:</h4>" +
+      "<ul style='list-style-type:none;margin-left:0;padding-left:0;'>";
+
+  $("main h2, main h3").each(function() {
+    var current = $(this)
+    var name = current.text();
+    current.attr("id", name)
+    current.attr('style', "padding-top:60px;margin-top: -60px")
+    ToC +=
+      "<li> <a href='#" + name + "'>" +
+        "&nbsp;".repeat(current.prop("tagName")[1]*4-6) + name +
+      "</a> </li>";
+  });
+
+  ToC +=
+   "</ul>" +
+  "</section>";
+
+  $("body").prepend(ToC);
+});
